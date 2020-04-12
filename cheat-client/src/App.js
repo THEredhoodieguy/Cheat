486be5c5f0b1abcfc78cf3c8a394671bef90ffac
Matt <sonicfan321@gmail.com>
2020-04-11 21:24:19 -0400

    Added banner for tracking turn state and pile size

--- cheat-client/src/App.js
+++ cheat-client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Hand from './components/Hand';
+import ActionBanner from './components/ActionBanner';
 
 import './App.css';
 
@@ -9,10 +10,13 @@ class App extends React.Component {
   render = () => {
     const opps = [{"name": "Steve", "handSize": 12}, {"name":"Dennis", "handSize":22}, {"name":"Dave", "handSize":8}];
     const hand = [{"suit":"♥️", "value":"ace"}, {"suit":"♠️", "value":"two"}, {"suit":"♦️", "value":"three"}, {"suit":"♣️", "value":"four"}];
+    //actionState: {currentPlayer: String, isDeciding: Boolean, numPlayed: Int, cardPlayed: String/cardEnum}
+    const currentState = {"currentPlayer": "Dennis", "isDeciding": true, "numPlayed": 0, "cardPlayed": "none"};
 
     return (
       <div className="app">
         <Header opponents={opps}/>
+        <ActionBanner pileSize={25} actionState={currentState} />
         <Hand cards={hand} />
       </div>
     );
---
+++ cheat-client/src/components/ActionBanner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+//a banner that displays the current state of the game
+class ActionBanner extends React.Component {
+    render = () => {
+        return (
+            <div className="actionBanner">
+                {/* Pile size */}
+                <div className="pileInfo">There are {this.props.pileSize} cards in the pile</div>
+                {/* 
+                 * Current player and their action 
+                 * 
+                 * actionState: {currentPlayer: String, isDeciding: Boolean, numPlayed: Int, cardPlayed: String/cardEnum}
+                */}
+                <div className="currentAction">It's {this.props.actionState.currentPlayer}'s turn. </div>
+            </div>
+        );
+    };
+}
+
+export default ActionBanner;
